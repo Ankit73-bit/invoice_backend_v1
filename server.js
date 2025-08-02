52962df15ae837a1acbc19d8e6077e8b627ab0eb
Ankit Bhuwad <124443636+Ankit73-bit@users.noreply.github.com>
2025-08-02 17:30:44 +0530

    Update
    - Update CORS origin

--- server.js
+++ server.js
@@ -15,10 +15,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://invoice-frontend-v1.vercel.app/",
-    ], // change this to your frontend URL in prod
+    origin: ["http://localhost:5173", "https://paras-invoice-tau.vercel.app"], // change this to your frontend URL in prod
     credentials: true, // if using cookies/auth headers
   })
 );
