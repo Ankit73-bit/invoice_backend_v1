4e5006c6fffb062a05293068d175d124864a9a7a
Ankit Bhuwad <124443636+Ankit73-bit@users.noreply.github.com>
2025-08-02 17:21:27 +0530

    Update
    - Added deployment URL to CORS

--- server.js
+++ server.js
@@ -15,7 +15,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // change this to your frontend URL in prod
+    origin: [
+      "http://localhost:5173",
+      "https://invoice-frontend-v1.vercel.app/",
+    ], // change this to your frontend URL in prod
     credentials: true, // if using cookies/auth headers
   })
 );
